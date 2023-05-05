5657c158d6588ed57c65b1fc7d90afaaa8103af7
Alexey <>
2023-05-05 23:37:40 +0500

    Перенёс проперти все в корневой каталог и тем самым починил настройку уровня доступа в пропертях

--- security/src/main/java/ru/home/security/FullAccessWebSecurityConfig.java
+++ security/src/main/java/ru/home/security/FullAccessWebSecurityConfig.java
@@ -1,19 +1,31 @@
 package ru.home.security;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConditionalOnProperty(value = "accessMode", havingValue = "FULL")
+@Order(0)
+@ConditionalOnProperty(prefix = "security", value = "accessMode", havingValue = "full")
+@EnableAutoConfiguration
 public class FullAccessWebSecurityConfig extends WebSecurityConfigBase {
+
+//    @Value("${security.accessMode}")
+//    private String securityAccessMode;
+
+    @Autowired
+    Environment environment;
 
     @Override
     protected void configure(@NotNull HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth ->
                 auth.anyRequest().permitAll());
         http.httpBasic().disable();
-        http.formLogin().disable();
+       http.formLogin().disable();
     }
 }
