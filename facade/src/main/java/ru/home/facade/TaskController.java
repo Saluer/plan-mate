dbbe56e320efef0e40c52dd0165a74bd5295940f
Alexey <>
2023-05-21 15:02:49 +0500

    Добавил cors с полным доступом, добавил отключение csrf - всё для fullAccess. Добавил сохранение таски

--- facade/src/main/java/ru/home/facade/TaskController.java
+++ facade/src/main/java/ru/home/facade/TaskController.java
@@ -3,14 +3,14 @@ package ru.home.facade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.home.model.Task;
 import ru.home.repository.TaskRepository;
 
 import java.util.Collection;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
     private final static Logger LOGGER = LoggerFactory.getLogger(TaskController.class);
 
@@ -23,6 +23,12 @@ public class TaskController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Collection<Task> getMyTasks() {
         return taskRepository.findAll();
+    }
+
+    @PutMapping(path = "save", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Task save(Task task) {
+        return taskRepository.save(task);
     }
 
 }
--- security/src/main/java/ru/home/security/FullAccessWebSecurityConfig.java
+++ security/src/main/java/ru/home/security/FullAccessWebSecurityConfig.java
@@ -2,9 +2,13 @@ package ru.home.security;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Component
 @ConditionalOnProperty(value = "security.accessMode", havingValue = "full")
@@ -16,10 +20,25 @@ public class FullAccessWebSecurityConfig extends WebSecurityConfigBase {
                 auth.anyRequest().permitAll());
         http.httpBasic().disable();
         http.formLogin().disable();
+        http.cors()
+                .and()
+                .csrf().disable();
     }
 
     @Override
     protected AuthenticationManager authenticationManager() {
         return null;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
